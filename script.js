8f1fae3b807cb86bc009ed759a21d0f656b92272
giteshpandit <panditgitesh38@gmail.com>
2024-09-18 15:12:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 const codes = document.querySelectorAll('.code');
 
-codes.forEach((input, index) => {
+input.forEach((input, index) => {
   input.addEventListener('input', () => {
-    if (input.value.length === 1 && index < codes.length - 1) {
-      codes[index + 1].focus();
+    if (input.value.length === 1 && index < input.length - 1) {
+      input[index + 1].focus();
     }
   });
 
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      if (input.value.length === 0 && index > 0) {
-        codes[index - 1].focus();
+  input.addEventListener('keydown', (event) => {
+    if (event.key === 'Backspace' && input.value.=== ''){
+      if (index > 0) {
+        input[index - 1].focus();
+        input[index - 1].value = '';
       }
     }
   });
-
-  // Set focus on first input when the page loads
-  if (index === 0) {
-    input.focus();
-  }
-});
