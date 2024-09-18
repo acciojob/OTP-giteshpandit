e94a1b1e502272b3895db4986a415df5dd6fd7f3
giteshpandit <panditgitesh38@gmail.com>
2024-09-18 15:17:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const codes = document.querySelectorAll('.code');
 
-input.forEach((input, index) => {
+// Function to move focus to the next input
+function moveToNextInput(currentInput, nextInput) {
+  if (currentInput.value.length === 1) {
+    nextInput.focus();
+  }
+}
+
+// Function to move focus to the previous input
+function moveToPreviousInput(currentInput, previousInput) {
+  if (currentInput.value === '') {
+    previousInput.focus();
+  }
+}
+
+codes.forEach((input, index) => {
+  // Event for typing in the input
   input.addEventListener('input', () => {
-    if (input.value.length === 1 && index < input.length - 1) {
-      input[index + 1].focus();
+    if (input.value.length === 1 && index < codes.length - 1) {
+      moveToNextInput(input, codes[index + 1]);
     }
   });
 
-  input.addEventListener('keydown', (event) => {
-    if (event.key === 'Backspace' && input.value.=== ''){
+  // Event for handling backspace
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
       if (index > 0) {
-        input[index - 1].focus();
-        input[index - 1].value = '';
+        moveToPreviousInput(input, codes[index - 1]);
       }
     }
   });
+
+  // Set focus on the first input when the page loads
+  if (index === 0) {
+    input.focus();
+  }
+});
